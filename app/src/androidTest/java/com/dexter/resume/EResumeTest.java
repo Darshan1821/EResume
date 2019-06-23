84c2ad9c552c3e1f0955771cb99cfe94ecb432a3
Darshan Bangoria <darshanbangoria.2@gmail.com>
2019-06-23 14:05:56 +0530

    Intial Setup

---
+++ app/src/androidTest/java/com/dexter/resume/EResumeTest.java
@@ -0,0 +1,21 @@
+package com.dexter.resume;
+
+import android.content.Context;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class EResumeTest {
+    @Test
+    public void useAppContext() {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.dexter.resume", appContext.getPackageName());
+    }
+}
---
+++ app/src/test/java/com/dexter/resume/EResumeUnitTest.java
@@ -0,0 +1,12 @@
+package com.dexter.resume;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EResumeUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
